722369ef887dca9dff50f4cfe89a945369b4d695
adityagururaj2000@gmail.com <Adi@191223>
2025-11-26 15:09:53 +0530

    Started CSS, JS

--- app/db/base.py
+++ app/db/base.py
@@ -1,2 +1,9 @@
 from sqlalchemy.orm import declarative_base
+from fastapi.templating import Jinja2Templates
+
+
 Base = declarative_base()
+
+templates = Jinja2Templates(directory='app/template')
+
+
--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1 import auth, habits
+from app.db.base import templates
+
+from fastapi.staticfiles import StaticFiles
 
 #Starts the App
 app = FastAPI(title="HabitGrid")
+
+app.mount('/static', StaticFiles(directory='app/static'), name='static')
 
 #Tells from which server to allow to modify the db,* means from anywhere
 app.add_middleware(
@@ -18,3 +23,7 @@ app.add_middleware(
 #Add all these endpoints into main.py
 app.include_router(auth.router)
 app.include_router(habits.router)
+
+@app.get("/")
+async def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
